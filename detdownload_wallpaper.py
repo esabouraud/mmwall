3128e1c660831d9c46f8fb08031b26ee9d0e0a2b
Eric Sabouraud <esabouraud@users.noreply.github.com>
2014-10-16 02:19:19 +0000

    split processing in 3: download images from DET, process image, upload or download on FTP server

---
+++ detdownload_wallpaper.py
@@ -0,0 +1,51 @@
+#!/bin/python
+# Download wallpapers from DET
+
+from bs4 import BeautifulSoup
+import urllib2
+from decimal import *
+import re
+import os
+
+URL = "http://deadendthrills.com/random/"
+OUTPUTDIR = "data"
+
+def save_image(imgurl):
+	m = re.match("^.*/(\\w*)/large/([\\w\\.]*$)", imgurl)
+	if (m == None):
+		print "No regexp match for %s" % imgurl
+		return
+	#print "%s_%s" % (m.group(1), m.group(2))
+	filepath = os.path.join("data", "%s_%s" % (m.group(1), m.group(2)))
+	print "Downloading %s as %s..." % (imgurl, filepath)
+	try:
+		response = urllib2.urlopen(imgurl)
+		f = open(filepath, "wb")
+		f.write(response.read())
+		print "Download completed"
+	except urllib2.HTTPError as e:
+		print e.read()
+		print "Download aborted"
+		return
+
+def get_wallpaper():
+	response = urllib2.urlopen(URL)
+	html = response.read()
+	
+	soup = BeautifulSoup(html)
+	#divs = soup.find_all("div", class_="jig-imageContainer")
+	div = soup.find("div", {"id": "content"}, {"role":"main"})
+	noscript = div.find("noscript")
+	aimgs = noscript.find_all("a")
+	for a in aimgs:
+		img = a.find("img")
+		imgwidth = int(img.get("width"))
+		imgheight = int(img.get("height"))
+		imgratio = round(Decimal(imgwidth) / Decimal(imgheight), 1)
+		# Only get images suitable for multiscreen wallpapers
+		if (imgratio >= 2.0):
+			save_image(a.get("href"))
+		#print "%s (%d, %d) %f" % (img.get('alt'), imgwidth, imgheight, imgratio)
+	
+if __name__=='__main__':
+	get_wallpaper()
--- synergy_wallpaper.py
+++ synergy_wallpaper.py
@@ -3,15 +3,13 @@
 
 from PIL import Image
 import ctypes
+import os
+import random
 
-INPUT = "D:\\Temp\\302943.jpg"
+INPUTDIR = "data"
+OUTPUTDIR = "current"
 OUTPUT_SCREENS = [(1280, 1024), (2560, 1024)]
 
-
-# set windows background
-def set_wallpaper(filepath):
-	SPI_SETDESKWALLPAPER = 20 
-	ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "right.jpg" , 0)
 
 # compute total virtual screen size
 def compute_screens_resolutions(screens):
@@ -31,13 +29,16 @@ def compute_screens_resolutions(screens):
 def compute_resize_resolution(imagewidth, imageheight, screenwidth, screenheight):
 	resizewidth = imagewidth
 	resizeheight = imageheight
+
 	if (screenwidth > resizewidth):
 		resizewidth = screenwidth
-		resizeheight *= screenwidth / imagewidth
+		resizeheight *= screenwidth
+		resizeheight /= imagewidth
 	if (screenheight > resizeheight):
-		resizewidth *= resizeheight / screenwidth
 		resizeheight = screenheight
-	
+		resizewidth *= resizeheight
+		resizewidth /= screenwidth
+
 	return (resizewidth, resizeheight)
 
 # crop center of image so that it matches screen dimensions
@@ -46,6 +47,7 @@ def compute_crop_coordinates(imagewidth, imageheight, screenwidth, screenheight)
 	y1 = 0
 	x2 = imagewidth
 	y2 = imageheight
+
 	if (screenwidth < imagewidth):
 		x1 = (imagewidth - screenwidth) / 2
 		x2 -= x1
@@ -56,7 +58,7 @@ def compute_crop_coordinates(imagewidth, imageheight, screenwidth, screenheight)
 	
 	return (x1, y1, x2, y2)
 
-def generate_wallpaper(inputimagepath, screenresolutions):
+def generate_wallpaper(inputimagepath, outputdir, screenresolutions):
 	(screenwidth, screenheight) = compute_screens_resolutions(screenresolutions)
 	print "Output screen resolution is: %d,%d)" % (screenwidth, screenheight)
 	
@@ -67,7 +69,7 @@ def generate_wallpaper(inputimagepath, screenresolutions):
 	(resizewidth, resizeheight) = compute_resize_resolution(imagewidth, imageheight, screenwidth, screenheight)
 	if (imagewidth != resizewidth or imageheight != resizeheight):
 		print "Upscaled image resolution is: %d,%d)" % (resizewidth, resizeheight)
-		im = im.resize(resizewidth, resizeheight, BICUBIC)
+		im = im.resize((resizewidth, resizeheight), Image.BICUBIC)
 	
 	if (screenwidth != resizewidth or screenheight != resizeheight):
 		(x1, y1, x2, y2) = compute_crop_coordinates(resizewidth, resizeheight, screenwidth, screenheight)
@@ -81,14 +83,11 @@ def generate_wallpaper(inputimagepath, screenresolutions):
 	for s in screenresolutions:
 		wall = im.crop((x, 0, x + s[0], s[1]))
 		wall.load()
-		wall.save("wall%d.jpg" % i)
+		wall.save(os.path.join(outputdir, "wall%d.jpg" % i))
 		x += s[0]
 		i += 1
 		
 	
 if __name__=='__main__':
-	generate_wallpaper(INPUT, OUTPUT_SCREENS)
-
-	
-	
-	
+	inputimagepath = os.path.join(INPUTDIR, random.choice(os.listdir(INPUTDIR)))
+	generate_wallpaper(inputimagepath, OUTPUTDIR, OUTPUT_SCREENS)
