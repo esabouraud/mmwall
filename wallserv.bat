75ea0b5b75872b8558c6e8dd2a75e3f3603af8e9
Eric Sabouraud <esabouraud@users.noreply.github.com>
2014-10-20 16:34:13 +0000

    Use SMB file sharing in batch instead of FTP in python for file transfer, add preconfigured desktops

--- wallserv.bat
+++ wallserv.bat
@@ -1,20 +1,25 @@
+@echo off
 @rem Do the heavy lifting
 @rem WALLFTP must be set (localhost, 192.168.1.1...)
 python detdownload_wallpaper.py -s
-python synergy_wallpaper.py -L
-python ftpsync_wallpaper.py -H %WALLFTP% -s -i 1
-python set_wallpaper.py -i 1
+python synergy_wallpaper.py -L -O %1
+@rem python ftpsync_wallpaper.py -H %WALLFTP% -s -i 1
+python set_wallpaper.py -i 1 -d current
 
 @rem Poor man's RPC to set wallpaper on client
 @rem WALLHOST, WALLUSER and WALLPASS must be set
 @rem using FTP to download wallpapers kinda pointless now...
 net use t: \\%WALLHOST%\C$\Temp /user:%WALLUSER% %WALLPASS%
-copy ftpsync_wallpaper.py t:
+@rem copy ftpsync_wallpaper.py t:
 copy set_wallpaper.py t:
-echo python ftpsync_wallpaper.py -H %WALLFTP% -i 0 > t:\wallcli.bat
-echo python set_wallpaper.py -i 0 >> t:\wallcli.bat
+rd /q /s t:\local
+md t:\local
+copy current\* t:\local
+@rem echo python ftpsync_wallpaper.py -H %WALLFTP% -i 0 > t:\wallcli.bat
+echo python set_wallpaper.py -i 0 > t:\wallcli.bat
 psexec \\%WALLHOST% -u %WALLUSER% -p %WALLPASS% -i -w "C:\Temp" "C:\Temp\wallcli.bat"
-del t:\ftpsync_wallpaper.py
-del t:\set_wallpaper.py t:
-del t:\wallcli.bat
+@rem del t:\ftpsync_wallpaper.py
+del /q t:\set_wallpaper.py t:
+del /q t:\wallcli.bat
 net use t: /d
+@echo on
