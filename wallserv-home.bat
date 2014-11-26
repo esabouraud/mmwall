4ede36efaa0c505bf766897ca1eed712c49b450f
Eric Sabouraud <esabouraud@users.noreply.github.com>
2014-11-26 17:53:29 +0000

    surround using on client the current user of the server (avoids writing cleartext password)

--- wallserv-home.bat
+++ wallserv-home.bat
@@ -1,4 +1,7 @@
-set WALLHOST=hostname
-set WALLUSER=domain\username
-@set WALLPASS=pass
-wallserv.bat "[(1366, 768, 550), (1280, 1024, 0)]" "(1280, 1024)"
+set "WALLHOST=hostname"
+set "WALLUSER=domain\username"
+@set "WALLPASS=pass"
+wallserv.bat "[(1366, 768, 550, 0), (1280, 1024, 0, 1)]" "(1280, 1024)"
+set "WALLHOST="
+set "WALLUSER="
+set "WALLPASS="
--- wallserv-work.bat
+++ wallserv-work.bat
@@ -1,4 +1,3 @@
-set WALLHOST=hostname
-set WALLUSER=domain\username
-@set WALLPASS=pass
-wallserv.bat "[(1280, 1024, 0), (2560, 1024, 0)]" "(1280, 1024)"
+set "WALLHOST=frws1484"
+wallserv.bat "[(1280, 1024, 0, 0), (2560, 1024, 0, 1)]" "(1280, 1024)"
+set "WALLHOST="
